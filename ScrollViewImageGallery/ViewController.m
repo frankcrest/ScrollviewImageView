1b203698e6be9dc19e5d31ca039d53bf382f8ff9
Frank Chen <frankcrestview@hotmail.com>
2019-05-13 14:47:34 -0400

    initial commit

--- ScrollViewImageGallery/ViewController.m
+++ ScrollViewImageGallery/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -16,7 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView* firstImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"infield"]];
+    firstImageView.contentMode = UIViewContentModeScaleAspectFit;
+    UIImageView* secondImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"lighthouse"]];
+    secondImageView.contentMode = UIViewContentModeScaleAspectFit;
+    UIImageView* thirdImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"night"]];
+    thirdImageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    UIStackView* stackView =[[UIStackView alloc]init];
+    stackView.axis = UILayoutConstraintAxisHorizontal;
+    stackView.distribution = UIStackViewDistributionFillEqually;
+    
+    [stackView addArrangedSubview:firstImageView];
+    [stackView addArrangedSubview:secondImageView];
+    [stackView addArrangedSubview:thirdImageView];
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:stackView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+                                              [firstImageView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:1],
+                                               [secondImageView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:1],
+                                               [thirdImageView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:1],
+                                              [firstImageView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:1],
+                                              [secondImageView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:1],
+                                              [thirdImageView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:1],
+                                              [stackView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+                                              [stackView.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
+                                              ]];
+    
+    
 }
 
 
